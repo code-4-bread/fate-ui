5b3f6bb7c4be0c0ba44b8d3cfb7525a430a79085
Code4Bread <iz@kaungmyat.tech>
2020-04-25 17:56:29 +0800

    nav bar added

--- src/pages/Header/Header.jsx
+++ src/pages/Header/Header.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
-import { Main, Heading, Paragraph } from 'grommet';
+import {
+  Main, Heading, Paragraph, Grid, Box, Button,
+} from 'grommet';
 import styled from 'styled-components';
 import { Colors } from '../../theme/theme';
 
@@ -18,10 +20,44 @@ const StyledParagraph = styled(Paragraph)`
   color: ${Colors.accent}
 `;
 
+const NavIcons = ({ ...rest }) => (
+  <Button plain focusIndicator={false} {...rest} />
+);
+
+const StyledButton = styled(NavIcons)`
+  font-weight: bold
+`;
+
 const Header = () => (
   <StyledMain>
     <StyledHeading alignSelf="center">FATE</StyledHeading>
     <StyledParagraph alignSelf="center">Scrum Poker Tool</StyledParagraph>
+    <Grid
+      areas={[
+        { name: 'menu-1', start: [0, 0], end: [0, 0] },
+        { name: 'menu-2', start: [1, 0], end: [1, 0] },
+        { name: 'menu-3', start: [2, 0], end: [2, 0] },
+      ]}
+      columns={['flex', 'flex', 'flex']}
+      rows={['flex']}
+      gap="large"
+    >
+      <Box gridArea="menu-1">
+        <StyledButton
+          label="Home"
+        />
+      </Box>
+      <Box gridArea="menu-2">
+        <StyledButton
+          label="History"
+        />
+      </Box>
+      <Box gridArea="menu-3">
+        <StyledButton
+          label="About"
+        />
+      </Box>
+    </Grid>
   </StyledMain>
 );
 
