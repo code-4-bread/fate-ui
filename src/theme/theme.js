64da576f599e892ffd0cce6e4edf288080157a2c
Code4Bread <iz@kaungmyat.tech>
2020-04-26 21:31:43 +0800

    theme updated

--- src/theme/theme.js
+++ src/theme/theme.js
@@ -1,15 +1,26 @@
 import { deepMerge } from 'grommet/es6/utils';
 import { grommet } from 'grommet/themes';
 
+const primaryColor = '#476A6F';
+const secondaryColor = '#7EB09B';
 
 const theme = deepMerge(grommet, {
   global: {
     colors: {
-      brand: '#2f4858',
-      accent: '#33658a',
+      brand: primaryColor,
+      accent: secondaryColor,
+      focus: primaryColor,
     },
     font: {
       family: 'Roboto',
+    },
+  },
+  tabs: {
+    gap: 'xlarge',
+  },
+  tab: {
+    active: {
+      weight: 'bold',
     },
   },
 });
